0c863864bdac268521ccc7b07544c961cefcf638
Arjun-tp <arjuntpnambiar@gmail.com>
2024-03-03 16:47:48 -0500

    firstOccurenceStringIndex

---
+++ firstOccurenceStringIndex.js
@@ -0,0 +1,27 @@
+/*
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+*/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    return haystack.indexOf(needle)
+};
+
+
+//Example
+// strStr("leetcode", "leeto")
