484f5b00384d6f1e22108ad5747c1398b70f3658
Arjun-tp <arjuntpnambiar@gmail.com>
2024-03-03 16:13:15 -0500

    lengthOfLastWord

---
+++ lengthOfLastWord.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    let words = s.split('').reverse().join('').split(' ')
+    return words.filter(word => word!== "")[0].length;
+};
+
+
+// Example
+// lengthOfLastWord("luffy is still joyboy")
