ceefde644c8df05c752d6fbe835f1228da0f4441
Arjun-tp <arjuntpnambiar@gmail.com>
2024-03-03 18:21:33 -0500

    removeDuplicatesII

---
+++ removeDuplicatesII.js
@@ -0,0 +1,38 @@
+/*
+Example 1:
+
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Example 2:
+
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let counter = 1
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                counter++
+                if (counter > 2) {
+                    nums.splice(i, 1)
+                    j--
+                }
+            }
+        }
+    }
+};
+
+
+// Example
+// removeDuplicates([1,1,1,2,2,3])
