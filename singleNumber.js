94b575515b4d6cbaa274c67d6a1aee9a7feb31c5
Arjun <apuliyullaveetil@viralnation.com>
2025-01-06 21:05:46 -0500

    single number

---
+++ singleNumber.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+    nums = nums.sort((a, b) => a - b)
+    for (let i = 0; i <= nums.length; i += 2) {
+        if (nums[i] !== nums[i + 1]) {
+            return nums[i]
+        }
+    }
+};
+
+// Example 1:
+//
+// Input: nums = [2,2,1]
+// Output: 1
+// Example 2:
+//
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+// Example 3:
+//
+// Input: nums = [1]
+// Output: 1
