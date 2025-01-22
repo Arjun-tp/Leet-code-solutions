235617f48abee024115782351ae89210f7499ebb
Arjun <apuliyullaveetil@viralnation.com>
2025-01-21 21:47:57 -0500

    feat: contains duplicate

---
+++ containsDuplicate.js
@@ -0,0 +1,34 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i + 1]) {
+            return true
+        }
+    }
+    return false
+};
+
+/*
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation: The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation: All elements are distinct.
+*/
