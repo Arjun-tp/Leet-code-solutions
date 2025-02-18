644b45f322b03943621b3008e26d585462988f48
Arjun-tp <arjuntpnambiar@gmail.com>
2025-02-18 11:50:28 -0500

    feat(lc-1768): merge strings alternately

---
+++ mergeStringsAlternatively.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+    let mergedArray = []
+    for (let i = 0; i <= word1.length - 1 || i <= word2.length - 1; i++) {
+        mergedArray.push(word1[i])
+        for (let j = 0; j <= word2.length - 1; j++) {
+            mergedArray.push(word2[i])
+            break
+        }
+    }
+    return mergedArray.join('')
+
+};
+
+
+
+/*
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+*/
