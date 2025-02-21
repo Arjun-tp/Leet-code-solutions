c77db951317073327561df740c64c570f3351c3b
Arjun-tp <arjuntpnambiar@gmail.com>
2025-02-20 21:17:09 -0500

    fix: fixed linters

--- smallestLetterGreaterThanTarget.js
+++ smallestLetterGreaterThanTarget.js
@@ -43,8 +43,6 @@ var nextGreatestLetter = function (letters, target) {
 You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
 Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
 
- 
-
 Example 1:
 Input: letters = ["c","f","j"], target = "a"
 Output: "c"
